f213e8d4d785d3ecf1d02065f93b5183201ab301
unknown <achilihudaniel53@gmail.com>
2022-07-15 10:09:40 +0100

    initial commit

---
+++ printhighestnum.js
@@ -0,0 +1,12 @@
+// Write a javascript code to print the highest number in the array.
+
+    var numbers=[-2, -4,  2, 45, 3, 68, 93, 299, 563, 6372, 954, 234,-5,]
+    var largest =numbers[0]
+
+    for (var i =0; i <numbers.length;  i++){
+    if (largest < numbers[i]) 
+    {largest =numbers[i]}
+    }   
+
+    console.log (largest)
+    
